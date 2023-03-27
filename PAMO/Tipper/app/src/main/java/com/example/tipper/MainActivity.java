9d0bd142185022b2bd4a57ab6e4393b62a86f78e
s21488-pj <s21488@pjwstk.edu.pl>
2023-03-27 22:19:38 +0200

    without recipes

--- PAMO/Tipper/app/src/main/java/com/example/tipper/MainActivity.java
+++ PAMO/Tipper/app/src/main/java/com/example/tipper/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.tipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText; // for bill amount input
-import android.widget.SeekBar; // for changing the tip percentage
-import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView; // for displaying text
 import java.lang.Math;
 import java.text.NumberFormat; // for currency formatting
@@ -17,16 +21,19 @@ public class MainActivity extends AppCompatActivity {
     // currency and percent formatter objects
     private static final NumberFormat currencyFormat =
             NumberFormat.getInstance();
+    final Handler handler = new Handler();
+    private double weight = 0.0; // bill amount entered by the user
+    private double height = 0.0; // bill amount entered by the user
+    private int age = 0;
 
-    private double billAmount = 0.0; // bill amount entered by the user
-    private double billAmount2 = 0.0; // bill amount entered by the user
-
-    private TextView amountTextView; // shows formatted bill amount
-    private TextView amountTextView2; // shows formatted bill amount
-
-
+    private TextView weightTextView; // shows formatted bill amount
+    private TextView heightTextView; // shows formatted bill amount
+    private RadioGroup genderGroup;
+    private RadioButton genderMan;
+    private RadioButton genderWoman;
     private TextView totalTextView; // shows calculated total bill amount
-
+    private TextView calCalc; // shows calculated total bill amount
+    private TextView ageTextView;
     // called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +41,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); // inflate the GUI
 
         // get references to programmatically manipulated TextViews
-        amountTextView = (TextView) findViewById(R.id.amountTextView);
-        amountTextView2 = (TextView) findViewById(R.id.amountTextView2);
-
-
+        weightTextView = (TextView) findViewById(R.id.weight);
+        heightTextView = (TextView) findViewById(R.id.height);
+        genderGroup = (RadioGroup) findViewById(R.id.genderGroup);
+        genderMan = (RadioButton) findViewById(R.id.radioMan);
+        genderWoman = (RadioButton) findViewById(R.id.radioWoman);
+        calCalc = (TextView) findViewById(R.id.calCalc);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
-
+        ageTextView = (TextView) findViewById(R.id.ageTextView);
         totalTextView.setText(currencyFormat.format(0));
-
+        calCalc.setText(currencyFormat.format(0));
+//
         // set amountEditText's TextWatcher
         EditText amountEditText =
-                (EditText) findViewById(R.id.amountEditText);
-        amountEditText.addTextChangedListener(amountEditTextWatcher);
+                (EditText) findViewById(R.id.weightEdit);
+        amountEditText.addTextChangedListener(weightWatcher);
 
         EditText amountEditText2 =
-                (EditText) findViewById(R.id.amountEditText2);
-        amountEditText2.addTextChangedListener(amountEditTextWatcher2);
+                (EditText) findViewById(R.id.heightEdit);
+        amountEditText2.addTextChangedListener(heightWatcher);
 
+        EditText amountEditText3 =
+                (EditText) findViewById(R.id.ageEdit);
+        amountEditText3.addTextChangedListener(ageWatcher);
+
+        RadioButton amountEditText4 =
+                (RadioButton) findViewById(R.id.radioMan);
+        amountEditText4.addTextChangedListener(genderWatcher);
 
 
 
@@ -63,31 +80,38 @@ public class MainActivity extends AppCompatActivity {
         // format percent and display in percentTextView
 
         // calculate the tip and total
-
-        double total = billAmount / Math.pow(billAmount2,2);
-
+        double calories = 0;
+        double total = weight / Math.pow(height,2);
+        if(genderMan.isChecked()){
+            calories = 66.47 + (13.7 * weight) + ( 5 * height * 100) - 6.76 * age;
+        }else if(genderWoman.isChecked())
+            calories = 655.1 + (9.567 * weight) + (1.85 * height * 100) - 4.68 * age;
         // display tip and total formatted as currency
 
         totalTextView.setText(currencyFormat.format(total));
+
+        calCalc.setText(currencyFormat.format(calories));
+
+
     }
 
     // listener object for the SeekBar's progress changed events
 
 
     // listener object for the EditText's text-changed events
-    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+    private final TextWatcher weightWatcher = new TextWatcher() {
         // called when the user modifies the bill amount
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count) {
 
             try { // get bill amount and display currency formatted value
-                billAmount = Double.parseDouble(s.toString())/100.0;
-                amountTextView.setText(currencyFormat.format(billAmount));
+                weight = Double.parseDouble(s.toString())/100.0;
+                weightTextView.setText(currencyFormat.format(weight));
             }
             catch (NumberFormatException e) { // if s is empty or non-numeric
-                amountTextView.setText("");
-                billAmount = 0.0;
+                weightTextView.setText("");
+                weight = 0.0;
             }
 
             calculate(); // update the tip and total TextViews
@@ -100,19 +124,19 @@ public class MainActivity extends AppCompatActivity {
         public void beforeTextChanged(
                 CharSequence s, int start, int count, int after) { }
     };
-    private final TextWatcher amountEditTextWatcher2 = new TextWatcher() {
+    private final TextWatcher heightWatcher = new TextWatcher() {
         // called when the user modifies the bill amount
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count) {
 
             try { // get bill amount and display currency formatted value
-                billAmount2 = Double.parseDouble(s.toString()) / 100.0;
-                amountTextView2.setText(currencyFormat.format(billAmount2));
+                height = Double.parseDouble(s.toString()) / 100.0;
+                heightTextView.setText(currencyFormat.format(height));
             }
             catch (NumberFormatException e) { // if s is empty or non-numeric
-                amountTextView2.setText("");
-                billAmount2 = 0.0;
+                heightTextView.setText("");
+                height = 0.0;
             }
 
             calculate(); // update the tip and total TextViews
@@ -125,21 +149,53 @@ public class MainActivity extends AppCompatActivity {
         public void beforeTextChanged(
                 CharSequence s, int start, int count, int after) { }
     };
+
+    private final TextWatcher ageWatcher = new TextWatcher() {
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try { // get bill amount and display currency formatted value
+                age = Integer.parseInt(s.toString());
+                ageTextView.setText(currencyFormat.format(age));
+            }
+            catch (NumberFormatException e) { // if s is empty or non-numeric
+                ageTextView.setText("1");
+                age = 1;
+            }
+
+            calculate(); // update the tip and total TextViews
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) { }
+    };
+
+    private final TextWatcher genderWatcher = new TextWatcher() {
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+
+
+            calculate(); // update the tip and total TextViews
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) { }
+    };
+
 
 }
 
 
-/*************************************************************************
- * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
